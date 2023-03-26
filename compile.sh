cfa923c9c2776af9a2a04b497f2dfff51572c7f7
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-26 23:14:32 +0200

    feat(main) debug flag

--- compile.sh
+++ compile.sh
@@ -5,8 +5,20 @@
 ## compile
 ##
 
-mkdir -p build
-cmake -S . -B build
-cmake --build build
-mv build/neo-neofetch .
-env echo -e "\e[1;32m[+] \e[0mCompiled successfully, binary is in the root directory."
+if [ $# == 0 ]; then
+    mkdir -p build
+    cmake -S . -B build
+    cmake --build build
+    mv build/neo-neofetch .
+    env echo -e "\e[1;32m[+] \e[0mCompiled successfully, binary is in the root directory."
+else
+    if [ $1 == "debug" ]; then
+        mkdir -p build
+        cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
+        cmake --build build
+        mv build/neo-neofetch .
+        env echo -e "\e[1;32m[+] \e[0mCompiled successfully, binary is in the root directory."
+        exit 0
+    fi
+    env echo -e "\e[1;31m[-] \e[0mInvalid argument."
+fi
